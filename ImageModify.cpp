5289416379124085202cf844f890d539731d6f51
David <dluna777@outlook.com>
2024-10-01 12:11:34 -0700

    initial commit

--- ImageModify.cpp
+++ ImageModify.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+void processHeader(){
+
+}
+
+void processBody(){
+
+}
+
+void PrintWriter(){
+    
+}
 int main(){
     printf("Hello, World!");
     return 0;
---
+++ Modification.cpp
@@ -0,0 +1,6 @@
+enum Modificaion{
+    NEGATE = 0, 
+    QUANTIIZE = 1, 
+    GRAY_SCALE = 2, 
+    FLIP_HORIZONTAL = 3
+};
